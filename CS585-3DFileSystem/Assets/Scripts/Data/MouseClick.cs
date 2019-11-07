80f1d49953c03f94ad1ff6767ff879a2fd8e83fd
Imon Daneshmand <imondaneshmand@gmail.com>
2019-11-06 16:29:14 -0800

    Double Click

---
+++ CS585-3DFileSystem/Assets/Scripts/Data/MouseClick.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+public class MouseClick : MonoBehaviour, IPointerDownHandler
+{
+    float clicks = 0;
+    float clickStart = 0;
+    const float delay = 0.5f;
+    
+    public void OnPointerDown(PointerEventData data)
+    {
+        clicks++;
+        if(clicks == 1)
+        {
+            clickStart = Time.time;
+        }
+        if(clicks > 1 && (Time.time - clickStart) <= delay)
+        {
+            clicks = 0;
+            clickStart = 0;
+            Debug.Log("Double click detected");
+        } 
+        else if(clicks > 2 || Time.time - clickStart > delay)
+        {
+            clicks = 0;
+            clickStart = 0;
+            Debug.Log("Reset click");
+        }
+    }
+}
