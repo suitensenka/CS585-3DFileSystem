1e07900032dab09385a067e377e5b69431103301
Sunny Shah <sangatshah1@gmail.com>
2019-11-25 22:34:29 -0800

    Warning Popul Panel

---
+++ CS585-3DFileSystem/Assets/PopupCloser.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupCloser : MonoBehaviour
+{
+    public void CloseWarning(GameObject obj)
+	{
+		obj.SetActive(false);
+	}
+}
